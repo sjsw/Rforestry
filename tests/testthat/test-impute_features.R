e67dcd7c47fea5881d20df126162dfedce141aa2
theo-s <theo_s@sonic.net>
2021-03-14 20:18:51 -0700

    Change the test for impute features to be included in testthat suite

--- tests/testthat/test-impute_features.R
+++ tests/testthat/test-impute_features.R
@@ -1,5 +1,5 @@
-library(forestry)
-library(testthat)
+test_that("Tests if imputation for features works", {
+  context('Tests imputation for features')
 set.seed(4)
 #rm(iris)
 # Reordering the rows to test an edge case where factor variables are not in
@@ -17,8 +17,8 @@ x_with_miss[idx_miss_numeric, "Sepal.Width"] <- NA
 
 forest <- forestry(x_with_miss, y, ntree = 500, seed = 2, nthread = 1)
 imputed_x <- impute_features(forest, x_with_miss, seed = 2)
-expect_equal(sum(imputed_x$Species != x$Species), 3)
-expect_equal(mean(abs(x$Sepal.Width - imputed_x$Sepal.Width)), 0.0772336783986305)
+expect_equal(sum(imputed_x$Species != x$Species), 2)
+expect_equal(mean(abs(x$Sepal.Width - imputed_x$Sepal.Width)), 0.074894503323687369734, tolerance = 1e-6)
 
 # Testing mean imputation fallback:
 set.seed(1)
@@ -30,5 +30,8 @@ x_with_miss[idx_miss_numeric, "Sepal.Width"] <- NA
 
 forest <- forestry(x_with_miss, y, ntree = 2, seed = 2, nthread = 1)
 imputed_x <- impute_features(forest, x_with_miss, seed = 2, use_mean_imputation_fallback = TRUE)
-expect_equal(sum(imputed_x$Species != x$Species), 34)
-expect_equal(mean(abs(x$Sepal.Width - imputed_x$Sepal.Width)), 0.231564627359759)
+expect_equal(sum(imputed_x$Species != x$Species), 27)
+expect_equal(mean(abs(x$Sepal.Width - imputed_x$Sepal.Width)), 0.21616277820762141992, tolerance = 1e-6)
+
+
+})
