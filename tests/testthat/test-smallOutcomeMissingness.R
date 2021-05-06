dc7f90e7f1d1b83e8683153042e44db89ab0cf8c
Theo <theo_s@berkeley.edu>
2021-05-06 13:15:24 -0400

    Test for very small Y values to see if squaring the outcomes is causing underflow issues.

---
+++ tests/testthat/test-smallOutcomeMissingness.R
@@ -0,0 +1,45 @@
+library(testthat)
+test_that("Tests missing value splitting when Outcome values are close to zero", {
+  # Set seed for reproductivity
+  set.seed(24750371)
+
+
+  context("For different distance functions (abs() vs square()), we see if we have underflow problems")
+  x <- data.frame(V1 = runif(5000, min = -1e-10, max = 1e-10),
+                  V2 = runif(5000, min = -1e-10, max = 1e-10),
+                  V3 = runif(5000, min = -1e-10, max = 1e-10))
+
+  y <- 5.7*x$V3
+
+  print(summary(y))
+  print(summary(y^2))
+
+  # Take some missing examples
+  sample_idx_1 <- sample(1:5000, size = 2000, replace = FALSE)
+  x[sample_idx_1,1] <- NA
+
+  sample_idx_2 <- sample(1:5000, size = 2000, replace = FALSE)
+  x[sample_idx_2,2] <- NA
+
+
+  forest <- forestry(
+    x,
+    y,
+    nthread = 2,
+    seed = 2
+  )
+
+  # In this example, we speculate that if the forest starts sending missing
+  # observations in random directions, that will mess up the R^2
+
+  p <- predict(forest, x, seed = 12)
+  R_squared <- 1- (mean((p - y)^2))/(var(y))
+
+  print(R_squared)
+
+  # WHEN USING ABS(), R^2 is pretty high, should be able to catch most of the
+  # signal here despite a lot of missingness (should be > .65)
+
+  # predictions should be monotonically increasing
+  expect_gt(R_squared, 0.65)
+})
